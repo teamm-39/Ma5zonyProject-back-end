3ebf622735341895b8907536ded8072e5b89a5ec
teamm-39 <eslam.ex2003@gmail.com>
2024-12-12 03:43:29 +0200

    .

--- Ma5zonyProject/Controllers/UsersController.cs
+++ Ma5zonyProject/Controllers/UsersController.cs
@@ -9,7 +9,6 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Ma5zonyProject.Controllers
 {
-    [IgnoreAntiforgeryToken]
     [Route("api/[controller]")]
     [ApiController]
     public class UsersController : ControllerBase
